14856050bf5aab1e0bbe1e78743fa142ba2f96ac
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2024-03-06 19:54:13 +0700

    Modify unit tests

--- test/libcsc/string.cpp
+++ test/libcsc/string.cpp
@@ -1,29 +1,39 @@
-#include <libcsc/string/string.hpp>
+#include <libcsc/string.hpp>
 
 #include <gtest/gtest.h>
 
+#include <utility>
+
 using namespace csc;
 
-TEST(StringTest, DefaultConstructor) {
-  String s;
-  EXPECT_TRUE(s.empty());
+TEST(StringTest, Constructors) {
+  String s1;
+  EXPECT_TRUE(s1.empty());
+  String s2("hello");
+  EXPECT_EQ(s2.size(), 5);
 }
 
-TEST(StringTest, CharConstructor) {
-  String s("hello");
-  EXPECT_EQ(s.size(), 5);
+TEST(StringTest, MoveOperations) {
+  // Create the original strings
+  String s1("test1");
+  String s2("test2");
+
+  // Move the strings
+  String moved1(std::move(s1));
+  String moved2 = std::move(s2);
+
+  // Check the properties of the moved-to objects
+  EXPECT_EQ(moved1.size(), 5);
+  EXPECT_EQ(moved2.size(), 5);
 }
 
-TEST(StringTest, CopyConstructor) {
-  String original("test");
-  const String &copy(original);
-  EXPECT_EQ(copy.size(), original.size());
-}
-
-TEST(StringTest, CopyAssignmentOperator) {
-  String original("original");
-  const String &copy = original;
-  EXPECT_EQ(copy.size(), original.size());
+TEST(StringTest, CopyOperations) {
+  String s1("test1");
+  const String &copy1(s1);
+  EXPECT_EQ(copy1.size(), s1.size());
+  String s2("test2");
+  const String &copy2 = s2;
+  EXPECT_EQ(copy2.size(), s2.size());
 }
 
 TEST(StringTest, IndexOperator) {
@@ -37,19 +47,39 @@ TEST(StringTest, AtMethod) {
   EXPECT_THROW(s.at(5), std::out_of_range);
 }
 
-TEST(StringTest, ResizeMethod) {
-  String s("hello");
-  s.resize(3);
-  EXPECT_EQ(s.size(), 3);
-  s.resize(5, 'x');
-  EXPECT_EQ(s.size(), 5);
-  EXPECT_EQ(s[4], 'x');
+TEST(StringTest, SizeCapacityReserve) {
+  String s1;
+  s1.reserve(10);
+  EXPECT_GE(s1.capacity(), 10);
+  String s2("hello");
+  EXPECT_EQ(s2.size(), 5);
+  EXPECT_GE(s2.capacity(), 5);
 }
 
-TEST(StringTest, ReserveAndCapacity) {
+TEST(StringTest, Empty) {
   String s;
-  s.reserve(10);
-  EXPECT_GE(s.capacity(), 10);
+  EXPECT_TRUE(s.empty());
+  s.push_back('a');
+  EXPECT_FALSE(s.empty());
+}
+
+TEST(StringTest, ShrinkToFit) {
+  String s("hello");
+  s.reserve(20);
+  EXPECT_GE(s.capacity(), 20);
+  s.shrink_to_fit();
+  EXPECT_EQ(s.capacity(), 5);
+}
+
+TEST(StringTest, ResizeMethod) {
+  String s1("hello");
+  s1.resize(3);
+  EXPECT_EQ(s1.size(), 3);
+  std::cerr << s1.c_str() << '\n';
+  String s2("h");
+  s2.resize(5, 'i');
+  EXPECT_EQ(s2.size(), 5);
+  EXPECT_EQ(s2[4], 'i');
 }
 
 TEST(StringTest, PushBack) {
@@ -76,31 +106,6 @@ TEST(StringTest, BeginEnd) {
   String s("test");
   EXPECT_EQ(*s.begin(), 't');
   EXPECT_EQ(*(s.end() - 1), 't');
-}
-
-TEST(StringTest, Empty) {
-  String s;
-  EXPECT_TRUE(s.empty());
-  s.push_back('a');
-  EXPECT_FALSE(s.empty());
-}
-
-TEST(StringTest, Size) {
-  String s("hello");
-  EXPECT_EQ(s.size(), 5);
-}
-
-TEST(StringTest, Capacity) {
-  String s("hello");
-  EXPECT_GE(s.capacity(), 5);
-}
-
-TEST(StringTest, ShrinkToFit) {
-  String s("hello");
-  s.reserve(20);
-  EXPECT_GE(s.capacity(), 20);
-  s.shrink_to_fit();
-  EXPECT_EQ(s.capacity(), 5);
 }
 
 int main(int argc, char *argv[]) {
